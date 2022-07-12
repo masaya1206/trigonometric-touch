a2f452d6360f359a0f909b8c6e896122442ee8f0
masaya1206 <89019474+masaya1206@users.noreply.github.com>
2022-07-12 20:46:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#output01').innerHTML = html01;
     document.querySelector('#output02').innerHTML = html02;
     const touchArea = document.getElementById('#output01');
-//    document.getElementById('#info').style.visibility = "hidden";
+    document.getElementById('#info').style.visibility = 'hidden';
     // ボタンをクリックしたときの処理を追加
     elCalc.addEventListener('click', function() {
 	obj = triangles[Math.floor( Math.random() * triangles.length)];
