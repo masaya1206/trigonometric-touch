0696c6d3a525440d533a6936d0b8d887b00a4716
masaya1206 <89019474+masaya1206@users.noreply.github.com>
2022-08-05 19:23:19 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -69,15 +69,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //touchAreaのイベント
     touchArea.addEventListener('touchstart', function() {
-	html02 = '<img src=' + obj.ans + '>';
-//	html02 = '<H2="HERE"></H2>';
+//	html02 = '<img src=' + obj.ans + '>';
+	html02 = '<H2="HERE"></H2>';
 	document.querySelector('#output02').innerHTML = html02;
     });
 
     touchArea1.addEventListener('touchmove', function() {
         event.preventDefault();  // 画面スクロールを防止
 //	html02 = '<img src=' + obj.ans + '>';
-//	html02 = '<H2="HERE"></H2>';
+	html02 = '<H2="HERE"></H2>';
 //	document.querySelector('#output02').innerHTML = html02;
     });
  /*
